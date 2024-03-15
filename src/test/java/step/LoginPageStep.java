b4f5edadff532fe5ba9799fcc2829a42bfcbd1d6
Bareisha Darya <dariaboreysha@gmail.com>
2024-03-15 15:41:49 +0300

    -added headless mode

--- src/test/java/step/LoginPageStep.java
+++ src/test/java/step/LoginPageStep.java
@@ -1,36 +1,47 @@
 package step;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 
+import java.util.function.BooleanSupplier;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class LoginPageStep {
 
-    @Given("I am on login page")
+    @Given("I am on basic page of website")
     public void openWebSite() {
-        assertEquals(WebDriverRunner.url(), "https://www.saucedemo.com/");
+        assertEquals(WebDriverRunner.url(), "https://www.demoblaze.com/");
     }
 
     @And("I click on submit button")
     public void clickOnSubmitButton() {
-        Selenide.$("#login-button").click();
+       $(By.xpath(".//button[@onclick='logIn()']")).click();
     }
 
     @When("I enter {} and {}")
     public void iEnterAnd(String username, String password) {
-        Selenide.$("#user-name").setValue(username);
-        Selenide.$("#password").setValue(password);
+        $("#loginusername").setValue(username);
+        $("#loginpassword").setValue(password);
     }
-    @Then("I get on Products page")
-    public void checkingPagePresence() {
-        assertEquals(Selenide.$(".title").text(), "Products");
+    @Then("I am logged in with my {}")
+    public void checkingPagePresence(String username) throws InterruptedException{
+        assertTrue($("a#nameofuser.nav-link").shouldBe(Condition.visible).getText().contains(username));
     }
 
 
+    @When("I click on Log in button")
+    public void iClickOnLogInButton() {
+        $("#login2").click();
+    }
 }
--- src/test/resources/scenarios/LoginTest.feature
+++ src/test/resources/scenarios/LoginTest.feature
@@ -3,15 +3,16 @@ Feature: User Login
   User should be able to get logged on the login page
 
 Background: I am on the main page of the website
-  Given Open website "https://www.saucedemo.com/"
+  Given Open website "https://www.demoblaze.com/"
 
 
   Scenario Outline: Login on the page with valid credentials
-    Given I am on login page
-    When I enter <username> and <password>
+    Given I am on basic page of website
+    When I click on Log in button
+    And I enter <username> and <password>
     And I click on submit button
-    Then I get on Products page
+    Then I am logged in with my <username>
 
     Examples:
-      | username      | password     |
-      | standard_user | secret_sauce |
+      | username | password |
+      | 1        | 1        |
