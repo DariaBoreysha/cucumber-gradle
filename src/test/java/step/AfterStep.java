adfe4fb37728efcd7db01154b5beffc28947b104
Bareisha Darya <dariaboreysha@gmail.com>
2024-03-20 13:13:07 +0300

    -eliminated bug

--- src/test/java/step/AfterStep.java
+++ src/test/java/step/AfterStep.java
@@ -6,10 +6,10 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class AfterStep {
 
-  /*  @AfterAll
-    public static void tearDown() throws InterruptedException{
+    @AfterAll
+    public static void tearDown(){
         closeWebDriver();
-    }*/
+    }
 
 /*    @io.cucumber.java.AfterStep
     public void makeScreenshot(){
