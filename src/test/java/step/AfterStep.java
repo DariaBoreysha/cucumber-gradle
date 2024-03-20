d82b44934c67a406c8e9742acdc12dc2f95bee45
Bareisha Darya <dariaboreysha@gmail.com>
2024-03-20 11:43:53 +0300

    -eliminated bug

--- src/test/java/step/AfterStep.java
+++ src/test/java/step/AfterStep.java
@@ -3,11 +3,14 @@ package step;
 import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.java.After;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.sleep;
+
 public class AfterStep {
 
     @After
-    public void tearDown(){
-        WebDriverRunner.getWebDriver().close();
+    public void tearDown() throws InterruptedException{
+        closeWebDriver();
     }
 
 /*    @io.cucumber.java.AfterStep
