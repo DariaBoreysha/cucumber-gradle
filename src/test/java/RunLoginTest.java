2d76f133940613a5b05a75f88a8df523141f3566
Darya Bareisha <dariaboreysha@gmail.com>
2024-05-13 14:42:41 +0300

    added commentary new

--- src/test/java/RunLoginTest.java
+++ src/test/java/RunLoginTest.java
@@ -2,7 +2,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.RunWith;
-//added smth
+//added smth new
 @Slf4j
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:reports/test-report.html","json:reports/cucumber-report.json"})
